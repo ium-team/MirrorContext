70f870131079d96dbe76a2d2080781937ce0b047
hefxpzwk <hefxpzwk@gmail.com>
2026-03-15 18:36:46 +0900

    이것저것 수정

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import rehypeSlug from 'rehype-slug';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mirrorcontext.ium.dev',
   markdown: {
     remarkPlugins: [remarkGfm],
     rehypePlugins: [rehypeSlug]
